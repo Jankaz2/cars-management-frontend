93947028e2342c3e531f518f107c7839fd059304
jankaz <jk@wp.pl>
2021-04-18 13:58:15 +0200

    Add CopyWebpackPlugin, modify html plugins, add output, entry and modify rules

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,6 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 //const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-/*import path from 'path'
-import HtmlWebPackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import {CleanWebpackPlugin} from 'clean-webpack-plugin'
-import CopyWebpackPlugin from 'copy-webpack-plugin'*/
 
 module.exports = {
     entry: {
