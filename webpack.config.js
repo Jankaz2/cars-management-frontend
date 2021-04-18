7481499480cbfd18bef7d6db1c6d3e7013df00af
jankaz <jk@wp.pl>
2021-04-18 13:55:11 +0200

    Add CopyWebpackPlugin, modify html plugins, add output, entry and modify rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
+const path = require('path')
+
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+/*import path from 'path'
+import HtmlWebPackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import {CleanWebpackPlugin} from 'clean-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'*/
 
 module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: ['babel-loader']
             },
             {
                 test: /\.html$/,
@@ -21,10 +32,10 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader'
+                }
             },
             {
                 test: /\.(css|scss)$/,
@@ -37,15 +48,32 @@ module.exports = {
         ]
     },
 
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './js/[name].bundle.js'
+    },
+
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "index.html"
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
+            chunks: ['index']
         }),
+
+        //new CleanWebpackPlugin(),
 
         new MiniCssExtractPlugin({
             filename: 'name.css',
             chunkFilename: '[id].css'
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/images', to: './images' }
+            ]
         })
-    ]
+    ],
+
+    devtool: 'source-map',
+    mode: "development"
 }
