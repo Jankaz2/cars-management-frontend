ab7a62e19889d89f3d00b3c6470bc92963272d05
jankaz <jk@wp.pl>
2021-04-21 20:15:22 +0200

    Add script for select components fields

--- src/js/data/filterCarsData.js
+++ src/js/data/filterCarsData.js
@@ -3,6 +3,7 @@ import {showDataForCars} from "./showData";
 export default class FilterCars {
     static filterCarsByBtn() {
         const searchCarsBtn = document.querySelector('.submit-button');
+
         searchCarsBtn.addEventListener('click', (e) => {
             const model = document.getElementById('model-search').value;
             const minMileage = document.getElementById('mileage-from').value;
@@ -10,12 +11,14 @@ export default class FilterCars {
             const color = document.getElementById('color-search').value;
             const priceMin = document.getElementById('price-from').value;
             const priceMax = document.getElementById('price-to').value;
-            const components = document.getElementById('components-search').value;
+            const componentBox1Value = document.getElementById('components-search-1').value
+            const componentBox2Value = document.getElementById('components-search-2').value
+
             if (model.length === 0 && minMileage.length === 0 && maxMileage.length === 0 && color.length === 0 && priceMin.length === 0
-                && priceMax.length === 0 && components.length === 0) {
+                && priceMax.length === 0) {
                 showDataForCars('');
             }else {
-                showDataForCars(`/filter/${model.toUpperCase()}/${priceMin}/${priceMax}/${color.toUpperCase()}/${minMileage}/${maxMileage}/${components.toUpperCase()}`);
+                showDataForCars(`/filter/${model.toUpperCase()}/${priceMin}/${priceMax}/${color.toUpperCase()}/${minMileage}/${maxMileage}/${componentBox1Value.toUpperCase()},${componentBox2Value.toUpperCase()}`);
             }
             e.preventDefault();
         });
