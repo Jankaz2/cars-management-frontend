89952459b3640a1a300c61e6bc134bfa313c37ed
jankaz <jk@wp.pl>
2021-04-26 15:17:20 +0200

    Change responsive css to hamburger navigation, fix switching off registry box when login box is on

--- src/js/navigation/login.js
+++ src/js/navigation/login.js
@@ -24,6 +24,7 @@ export default class LoginBox {
                 registryHamburgerBox.style.display = 'none'
             } else {
                 loginHamburgerBox.style.display = 'block'
+                registryHamburgerBox.style.display = 'none'
             }
             e.preventDefault()
         })
