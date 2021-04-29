e00afb80d489500ca001f1fd3b5c6ed8610ac2d1
jankaz <jk@wp.pl>
2021-04-29 22:02:32 +0200

    Fix filter method

--- src/js/table_operations/filterCarsData.js
+++ src/js/table_operations/filterCarsData.js
@@ -13,12 +13,11 @@ export default class FilterCars {
             const priceMax = document.getElementById('price-to').value;
             const componentBox1Value = document.getElementById('components-search-1').value
             const componentBox2Value = document.getElementById('components-search-2').value
-
             if (model.length === 0 && minMileage.length === 0 && maxMileage.length === 0 && color.length === 0 && priceMin.length === 0
                 && priceMax.length === 0 && (componentBox1Value === 'component' && componentBox2Value === 'component2')) {
-                showDataForCars('');
+                showDataForCars(``)
             } else {
-                showDataForCars(`/filter/${model.length > 0 ? model.toUpperCase() : 'default-model'}/${priceMin.length > 0 ? priceMin : '0'}/${priceMax.length > 0 ? priceMax : '100000000'}/${color.length > 0 ? color.toUpperCase() : 'default-color'}/${minMileage.length > 0 ? minMileage : '0'}/${maxMileage.length > 0 ? maxMileage : '100000000'}/${componentBox1Value !== 'component' ? componentBox1Value.toUpperCase() : componentBox1Value},${componentBox2Value !== 'component2' ? componentBox2Value.toUpperCase() : componentBox2Value}`);
+                showDataForCars(`/filter/${model.length > 0 ? model.toUpperCase() : 'default-model'}/${priceMin.length > 0 ? priceMin : '0'}/${priceMax.length > 0 ? priceMax : 'max-price'}/${color.length > 0 ? color.toUpperCase() : 'default-color'}/${minMileage.length > 0 ? minMileage : '0'}/${maxMileage.length > 0 ? maxMileage : 'max-mileage'}/${componentBox1Value !== 'component' ? componentBox1Value.toUpperCase() : componentBox1Value},${componentBox2Value !== 'component2' ? componentBox2Value.toUpperCase() : componentBox2Value}`)
             }
             e.preventDefault();
         });
